7ffe0c5540a9f208562f4a063dd9819eb943249e
Anderson Botega <henriquebotega@gmail.com>
2020-12-05 18:25:09 -0800

    test add item to list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-	return <div className="App">app</div>;
+	const [value, setValue] = useState("");
+	const [items, setItems] = useState([]);
+
+	const handleAdd = () => {
+		setItems([...items, value]);
+		setValue("");
+	};
+
+	return (
+		<>
+			<ul>
+				{items.map((item, index) => (
+					<li key={index}>{item}</li>
+				))}
+			</ul>
+
+			<input
+				autoFocus
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+				placeholder="Add new item"
+			/>
+
+			<button onClick={handleAdd}>Add</button>
+		</>
+	);
 }
 
 export default App;
